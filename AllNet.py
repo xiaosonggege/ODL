3b2ed6582b255b66a15e0b6f350e6163e7313fc5
xiaosonggege <34638887+xiaosonggege@users.noreply.github.com>
2019-02-02 23:13:16 +0800

    Initial commit

---
+++ AllNet.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: songyunlong
+@license: (C) Copyright 2018-2021, Node Supply Chain Manager Corporation Limited.
+@contact: 1243049371@qq.com
+@software:PyCharm
+@file: AllNet
+@time: 2019/2/2 23:08
+@desc:
+'''
+
+import tensorflow as tf
+import numpy as np
+
+class NeuralNetwork:
+    pass
